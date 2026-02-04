8df03fd4ba260343e381777a705f5cf3c1666b9c
Polyoo <danialamirulbkp2021@gmail.com>
2026-02-04 16:16:55 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-GAMMA_API_URL = "https://gamma-api.polymarket.com/markets"
-SERIES_SLUG = "btc-up-or-down-15m"
-
-TRADE_AMOUNT_USD = 5
-THRESHOLD_CENT = 0.40
-SESSION_SECONDS = 900  # 15 min
-WS_RECONNECT_DELAY = 5  # detik
