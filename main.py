c49d21e03fbbe7e2b94fb831275edb5f0dd31ee1
Polyoo <danialamirulbkp2021@gmail.com>
2026-02-04 14:39:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import asyncio
 from core.polymarket_poll import PolymarketPoll
-from core.strategy import Strategy  # pastikan ada method check_price(up_price, down_price)
-from config import TRADE_AMOUNT_USD, THRESHOLD_CENT, SESSION_SECONDS
+from core.strategy import Strategy
+
+TRADE_AMOUNT_USD = 5
+THRESHOLD_CENT = 0.40
 
 async def bot_loop():
-    # Init polling bot & strategy
     poll = PolymarketPoll()
     strategy = Strategy(trade_amount=TRADE_AMOUNT_USD, threshold=THRESHOLD_CENT)
 
     def update_callback(up_price, down_price):
-        """
-        Trigger setiap kali price update
-        """
-        # Check logic kau: harga < threshold
         strategy.check_price(up_price, down_price)
 
-    # Start polling loop
     await poll.start_polling(update_callback)
 
 if __name__ == "__main__":
