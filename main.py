749b08738c2602c1b8fe01a5eccfa857344ae3c0
Polyoo <danialamirulbkp2021@gmail.com>
2026-02-04 11:12:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import asyncio
+from core.polymarket_ws import PolymarketWS
 
+MARKET_ID = "ISI_MARKET_ID_POLYMARKET"
+
+ws = PolymarketWS(MARKET_ID)
+
+async def run():
+    await ws.connect()
+
+asyncio.run(run())
