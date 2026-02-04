25a3c95cb2bbfed494414237a485dfe3508bc758
Polyoo <danialamirulbkp2021@gmail.com>
2026-02-04 13:59:55 +0800

    Update timer.py

--- core/timer.py
+++ core/timer.py
@@ -1,1 +1,12 @@
+import time
 
+class Timer:
+    def __init__(self, session_seconds=900):  # 15 min
+        self.session_seconds = session_seconds
+        self.start_time = time.time()
+
+    def is_session_over(self):
+        return (time.time() - self.start_time) >= self.session_seconds
+
+    def reset(self):
+        self.start_time = time.time()
