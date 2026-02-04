08642186d01d4d464cfaecd6715ead24d82099e2
Polyoo <danialamirulbkp2021@gmail.com>
2026-02-04 14:38:57 +0800

    Create polymarket_poll.py

---
+++ core/polymarket_poll.py
@@ -0,0 +1,30 @@
+import asyncio
+from core.polymarket_api import PolymarketAPI
+
+class PolymarketPoll:
+    def __init__(self):
+        self.api = PolymarketAPI()
+        self.market_id = None
+        self.up_id = None
+        self.down_id = None
+        self.last_up_price = 0
+        self.last_down_price = 0
+
+    async def start_polling(self, update_callback):
+        """Loop polling tiap 1s, panggil update_callback(up_price, down_price)"""
+        while True:
+            market_id, up_id, down_id = self.api.fetch_latest_market()
+            if market_id:
+                if market_id != self.market_id:
+                    print(f"🕒 New session started for Market {market_id}")
+                    self.market_id = market_id
+                    self.up_id = up_id
+                    self.down_id = down_id
+
+                prices = self.api.fetch_market_prices(market_id)
+                if prices:
+                    self.last_up_price = prices.get("Up", 0)
+                    self.last_down_price = prices.get("Down", 0)
+                    update_callback(self.last_up_price, self.last_down_price)
+
+            await asyncio.sleep(1)
