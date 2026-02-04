811afdb693b8557ac6d27dcfd5d26fdebf869100
Polyoo <danialamirulbkp2021@gmail.com>
2026-02-04 13:57:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+# config.py
 
+GAMMA_API_URL = "https://gamma-api.polymarket.com/markets"
+SERIES_SLUG = "btc-up-or-down-15m"
+
+TRADE_AMOUNT_USD = 5  # setiap trade
+THRESHOLD_CENT = 0.40  # trigger <0.40 cent
